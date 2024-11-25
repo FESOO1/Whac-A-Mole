c1d0dd566d51ddc17ab2093225a0c1c5985d8335
FES001 <firdavs7794948@gmail.com>
2024-11-25 21:36:56 +0500

    Working on it.

--- script.js
+++ script.js
@@ -5,9 +5,21 @@ const box = document.querySelectorAll('.box');
 window.addEventListener('DOMContentLoaded', startTheGame);
 
 function startTheGame() {
-    box[Math.floor(Math.random() * 3)].innerHTML = `
+    box[Math.floor(Math.random() * 4)].innerHTML = `
         <div class="pop-up">
             <img src="./assets/character.png" alt="character" class="pop-up-image">
         </div>`
     ;
+    const popUp = document.querySelector('.pop-up');
+    popUp.addEventListener('click', () => {
+        for (const boxes of box) {
+            boxes.innerHTML = '';
+        };
+        console.log('Caught');
+        box[Math.floor(Math.random() * 4)].innerHTML = `
+        <div class="pop-up">
+            <img src="./assets/character.png" alt="character" class="pop-up-image">
+        </div>`
+        ;
+    });
 };
