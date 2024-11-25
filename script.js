988fbffb263fd1e6895d1d1bf387936b0eb5aea7
FES001 <firdavs7794948@gmail.com>
2024-11-25 21:49:06 +0500

    Working on it.

--- script.js
+++ script.js
@@ -9,14 +9,16 @@ function startTheGame() {
     const popUp = document.createElement('div');
     popUp.classList.add('pop-up')
     popUp.innerHTML = `
-        <img src="./assets/character.png" alt="character" class="pop-up-image">
+        <img draggable="false" src="./assets/character.png" alt="character" class="pop-up-image">
     `;
     setInterval(() => {
+        popUp.classList.remove('shot');
         box[Math.floor(Math.random() * 4)].appendChild(popUp);
-    }, 300);
+    }, 500);
 
     popUp.addEventListener('click', () => {
         gamePoints += 100;
         console.log('Game Points: ' + gamePoints);
+        popUp.classList.add('shot');
     });
 };
